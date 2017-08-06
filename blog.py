e6eed3bf43bcf3425e08f16941fb1e82cbe5a823
ltoddy <taoliu14@acm.org>
2017-08-06 19:07:12 +0800

    Day02 Jinja2模板引擎以及继承Twitter Bootstrap

--- blog.py
+++ blog.py
@@ -1,12 +1,32 @@
 from flask import Flask
+from flask import render_template
+from flask_bootstrap import Bootstrap
+from flask_script import Manager
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+manager = Manager(app)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
+
+
+@app.route('/<username>')
+def user(username):
+    return render_template('user.html', name=username)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
