1f2431dc2070df899af9ecf9ef2c6687b1ebb645
ltoddy <taoliu14@acm.org>
2017-08-12 02:29:25 +0800

    Day04 Sqlite数据库

--- blog.py
+++ blog.py
@@ -10,11 +10,25 @@ from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms import SubmitField
 from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+# 我们要把数据库放在当下的文件夹,这个basedir就是当下文件夹的绝对路径
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+                                        os.path.join(basedir, 'data.sqlite')
+# 这里注意看，写的是URI（统一资源标识符）
+app.config['SQLALCHEMY_COMMIT_TEARDOWN'] = True
+# SQLALCHEMY_COMMIT_TEARDOWN 因为数据库每次有变动的时候，数据改变，但不会自动的去改变数据库里面的数据，
+# 只有你去手动提交，告诉数据库要改变数据的时候才会改变，这里配置这个代表着，不需要你手动的去提交了，自动帮你提交了。
+# 待会会有演示
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 bootstrap = Bootstrap(app)
 manager = Manager(app)
+db = SQLAlchemy(app)
 
 
 class NameForm(FlaskForm):
@@ -22,17 +36,33 @@ class NameForm(FlaskForm):
     submit = SubmitField('提交')
 
 
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    # 如果你学过数据库的话就知道我们一般通过id来作为主键，来找到对应的信息的,通过id来实现唯一性
+    name = db.Column(db.String(64), unique=True)
+
+    def __repr__(self):
+        return 'users表: id为:{}, name为:{}'.format(self.id, self.name)
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('你更改了名字')
+        user = User.query.filter_by(name=form.name.data)
+        if user is None:
+            user = User(name=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
         form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', name=session.get('name', None), form=form)
+    return render_template('index.html',
+                           form=form, name=session.get('name'),
+                           known=session.get('known', False))
 
 
 @app.route('/<username>')
